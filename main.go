82a750beaa72babe3058595ed6ae94e662a7f1e9
Eduards Bistrums <eduards.bistrums@gmail.com>
2016-03-23 17:51:25 +0200

    Added files via upload

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	var err error
+	tpl, err = template.ParseFiles("templates/index.gohtml")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", index)
+
+	fs := http.FileServer(http.Dir("public"))
+	http.Handle("/css/", fs)
+	http.Handle("/img/", fs)
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	tpl.Execute(w, nil)
+}
